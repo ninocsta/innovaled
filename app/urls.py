7ad016df120dcdb57233e053214d475deb56becf
Nicolas <nino-csta@hotmail.com>
2026-02-03 18:27:40 -0300

    ajustes nas configurações de arquivos estáticos e de mídia, adição da função para servir arquivos de contrato

--- app/urls.py
+++ app/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import include
 from django.contrib.auth import views as auth_views
-from .views import CustomLoginView
+from .views import CustomLoginView, servir_arquivo_contrato
 
 
 urlpatterns = [
@@ -13,6 +13,9 @@ urlpatterns = [
 
     path('login/', CustomLoginView.as_view(), name='login'),  # Usa a CustomLoginView aqui
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    path('contrato/arquivo/<int:documento_id>/', servir_arquivo_contrato, name='servir_arquivo_contrato'),
+
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- app/views.py
+++ app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import views as auth_views
+from django.http import FileResponse, HttpResponseForbidden
+from django.conf import settings
+from django.shortcuts import get_object_or_404
+import os
+from core.models import Contrato, DocumentoContrato
 
 
 class CustomLoginView(auth_views.LoginView):
@@ -13,3 +18,18 @@ def custom_404(request, exception):
     return render(request, '404.html', status=404)
 
 
+def servir_arquivo_contrato(request, documento_id):
+    documento = get_object_or_404(DocumentoContrato, pk=documento_id)
+
+    # Apenas usuários autenticados podem acessar
+    if not request.user.is_authenticated:
+        return HttpResponseForbidden("Você não tem permissão para acessar este arquivo.")
+
+    # Caminho do arquivo no servidor
+    file_path = os.path.join(settings.MEDIA_ROOT, documento.arquivo.name)
+
+    # Verificar se o arquivo existe
+    if not os.path.exists(file_path):
+        return HttpResponseForbidden("Arquivo não encontrado.")
+
+    return FileResponse(open(file_path, 'rb'))
